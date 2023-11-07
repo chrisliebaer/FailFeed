62c7a7981dd30f58fc8955fcebd4d3bba8d3b1a8
Chrisliebär <mail@chrisliebaer.de>
2023-11-07 04:20:00 +0100

    👷 Pull updates automatically
    This makes GitHub actions perform periodic updates of the addon by checking the ElitismHelper repository for changes. When changes are detected, a pull request is created and the addon is updated automatically.

---
+++ tools/update.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# This script is called by the GitHub action workflow periodically to check for new updates to EliteHelper.
+# If an update is found, the script will commit the changes. A later step in the workflow will create a pull request, if necessary.
+# If the hashes are different, there is an update and a PR needs to be updated, which will be done by the workflow.
+# The workflow will ensure that commits based on the same ElitismHelper commit are resulting in the same commit hash.
+
+EHDB_REPO="https://github.com/amki/ElitismHelper"
+EHDB_FILE="db/EHDB.lua"
+
+# redirect GIT_ENV to /dev/null on local runs
+GITHUB_ENV="${GITHUB_ENV:-/dev/null}"
+
+ehdb_git="$(mktemp -d /tmp/ehdb.XXXXXX)"
+trap 'rm -rf "$ehdb_git"' EXIT
+echo "Cloning ElitismHelper repository to $ehdb_git"
+git clone --depth=1 "$EHDB_REPO" "$ehdb_git"
+
+# Hash the current version database.
+old_hash="$(sha256sum "$EHDB_FILE" | cut -d' ' -f1)"
+
+# Fetch the latest version of ElitismHelper and extract the spell database.
+./tools/fetchDB.sh "$ehdb_git/ElitismHelper.lua" "$EHDB_FILE"
+
+new_hash="$(sha256sum "$EHDB_FILE" | cut -d' ' -f1)"
+
+echo "Old hash: $old_hash"
+echo "New hash: $new_hash"
+
+# If hashes are the same, there is no update.
+if [[ "$old_hash" == "$new_hash" ]]; then
+	echo "No update found. EliteHelper is up to date."
+	exit 0
+fi
+
+# If new hash matches the stored hash, there is an update, but a PR has already been created and we don't need to do anything.
+if [[ "$new_hash" == "$stored_hash" ]]; then
+	echo "Update found, but a pull request has already been created. Nothing to do."
+	exit 0
+fi
+
+# If we get here, there is an update and we need to commit the changes.
+echo "Update found. Committing changes."
+
+# At this point we need to collect some information from the ElitismHelper repository.
+# Since we are already inside a git repository, we need to tell git to use the ElitismHelper repository in the $ehdb_git directory.
+# Using same commit date as ElitismHelper to create the same commit hash even on multiple runs.
+EHDB_GIT_COMMIT="$(git --git-dir="$ehdb_git/.git" rev-parse HEAD)"
+EHDB_GIT_SHORT_COMMIT="$(git --git-dir="$ehdb_git/.git" rev-parse --short HEAD)"
+GIT_COMMITTER_DATE="$(git --git-dir="$ehdb_git/.git" show -s --format=%cI "$EHDB_GIT_COMMIT")"
+GIT_AUTHOR_DATE="$(git --git-dir="$ehdb_git/.git" show -s --format=%aI "$EHDB_GIT_COMMIT")"
+
+git add "$EHDB_FILE"
+git commit -m "🍱 Update EliteHelper database to $EHDB_GIT_SHORT_COMMIT"
+
+# Pass details to the GitHub action workflow.
+echo "pr_title=🍱 Update EliteHelper database to $EHDB_GIT_SHORT_COMMIT" >> "$GITHUB_OUTPUT"
+echo "pr_body=This PR updates the EliteHelper database to the latest version of ElitismHelper ($EHDB_GIT_SHORT_COMMIT)." >> "$GITHUB_OUTPUT"
