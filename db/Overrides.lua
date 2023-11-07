61f3f1a87afbd138fcad2e62b8e47c99c9edbde8
Chrisliebär <11184804+chrisliebaer@users.noreply.github.com>
2023-11-07 15:21:16 +0100

    🐛 Fix db overrides (#6)

--- db/Overrides.lua
+++ db/Overrides.lua
@@ -2,67 +2,65 @@
 -- It is intended to be used to override the default EHDB database.
 -- Tables are located in ns.ehdb.<TableName>
 -- All tables are maps, with the key being the spellID and the value being true.
--- If you want to add an entry, set the value to True.
--- If you want to remove an entry, set the value to Nil.
+-- If you want to add an entry, set the value to true.
+-- If you want to remove an entry, set the value to false.
 
 local _, ns = ...
 
-local tables = {
-	"Spells",
-	"SpellsNoTank",
-	"Auras",
-	"AurasNoTank",
-	"MeleeHitters",
-}
+local overrides = {}
 
-local Spells = {
-	[393444] = Nil, -- Spear Flurry / Gushing Wound (Refti Defender)
+overrides.Spells = {
+	[393444] = false, -- Spear Flurry / Gushing Wound (Refti Defender)
 
-	[393432] = True, -- Spear Flurry (Refti Defender)
-	[393444] = True, -- Gushing Wound (Refti Defender)
-	[256477] = True, -- Shark Toss (Trothak, Ring of Booty)
-	[183100] = True, -- Avalanche, Rocks (Mightstone Breaker)
-	[411001] = True, -- Lethal Current (Lurking Tempest)
+	[393432] = true, -- Spear Flurry (Refti Defender)
+	[393444] = true, -- Gushing Wound (Refti Defender)
+	[256477] = true, -- Shark Toss (Trothak, Ring of Booty)
+	[183100] = true, -- Avalanche, Rocks (Mightstone Breaker)
+	[411001] = true, -- Lethal Current (Lurking Tempest)
 
 	-- ... add entries here
 }
 
-local SpellsNoTank = {
-	[382712] = Nil, -- Necrotic Breath, Initial (Wilted Oak)
-	[382805] = Nil, -- Necrotic Breath, DoT (Wilted Oak)
-	[226406] = Nil, -- Ember Swip (Emberhusk Dominator)
+overrides.SpellsNoTank = {
+	[382712] = false, -- Necrotic Breath, Initial (Wilted Oak)
+	[382805] = false, -- Necrotic Breath, DoT (Wilted Oak)
+	[226406] = false, -- Ember Swip (Emberhusk Dominator)
 
-	[387571] = True, -- Focused Deluge (Primal Tsunami)
-	[387504] = True, -- Squall Buffet (Primal Tsunami)
-	[374544] = True, -- Burst of Decay (Fetid Rotsinger)
-	[385833] = True, -- Bloodthirsty Charge (Rageclaw) (Knockback)
-	[385834] = True, -- Bloodthirsty Charge (Rageclaw) (Dot)
-	[200732] = True, -- Molten Crash (Dargrul)
-
-	-- ... add entries here
-}
-
-local Auras = {
-	-- ... add entries here
-}
-
-local AurasNoTank = {
-	[374615] = True, -- Cheap Shot (Skulking Zealot)
+	[387571] = true, -- Focused Deluge (Primal Tsunami)
+	[387504] = true, -- Squall Buffet (Primal Tsunami)
+	[374544] = true, -- Burst of Decay (Fetid Rotsinger)
+	[385833] = true, -- Bloodthirsty Charge (Rageclaw) (Knockback)
+	[385834] = true, -- Bloodthirsty Charge (Rageclaw) (Dot)
+	[200732] = true, -- Molten Crash (Dargrul)
 
 	-- ... add entries here
 }
 
-local MeleeHitters = {
+overrides.Auras = {
 	-- ... add entries here
 }
 
+overrides.AurasNoTank = {
+	[374615] = true, -- Cheap Shot (Skulking Zealot)
+
+	-- ... add entries here
+}
+
+overrides.MeleeHitters = {
+	-- ... add entries here
+}
 
 function merge(target, override)
 	for k, v in pairs(override) do
-		target[k] = v
+		if v == false then
+			target[k] = nil
+		else
+			target[k] = v
+		end
 	end
 end
 
-for _, table in ipairs(tables) do
-	merge(ns.ehdb[table], _G[table])
+
+for table, _ in pairs(overrides) do
+	merge(ns.ehdb[table], overrides[table])
 end
